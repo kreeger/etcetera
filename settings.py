019ad62550a644da134e4526cb96de55d61acfeb
etc <educationaltechnologycenter@gmail.com>
2009-07-17 13:33:33 -0500

    Fixed login redirect issue on production server.

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
-PROD = False
+PROD = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
