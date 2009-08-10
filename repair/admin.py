57dabcc575eba13b8e416603f80d72d2d6715c47
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-08-10 14:39:52 -0500

    Fixed problem with admin display fields.

--- repair/admin.py
+++ repair/admin.py
@@ -6,8 +6,6 @@ class WorkOrderAdmin(admin.ModelAdmin):
 	raw_id_fields = ('equipment',)
 	list_display = ('last_name','first_name','creation_date','equipment_text','priority','work_type','archived',)
 	list_filter = ('work_type','priority','creation_date',)
-	list_display = ('last_name','first_name','creation_date','priority','work_type',)
-	list_filter = ('building','work_type','priority',)
 	search_fields = ('first_name','last_name','department','equipment__equipment_type__name','equipment_text','building__name','room','description','actions','technician__first_name','technician__last_name','technician__username','funding_source','work_type','budget',)
 
 # Register the appropriate models 
