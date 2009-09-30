2a9db586dc199a841d3c2c84a9b74427827405d8
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-09-30 14:24:37 -0500

    More checkout model creation.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+import datetime as dt
 
-# Create your models here.
+from django.db import models
+from django.contrib.localflavor.us import models as lfus
+from django.contrib.auth import models as auth
+
+from etcetera.structure import models as structure
+from etcetera.equipment import models as equipment
+
+class Checkout(models.Model):
+	"""An order for checking out equipment."""
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	department = models.ForeignKey(structure.OrganizationalUnit)
+	course = models.CharField(max_length=20, blank=True)
+	phone = lfus.PhoneNumberField()
+	email = models.EmailField(max_length=75)
+	equipment_needed = models.TextField()
+	building = models.ForeignKey(structure.Building, related_name='checkouts')
+	room = models.CharField(max_length=25)
+	checkout_type = models.CharField(
+		max_length=8,
+		choices=constants.CHECKOUT_TYPES
+	)
+	returner = models.CharField(max_length=9, choices=constants.RETURNERS)
+	creation_date = models.DateTimeField(default=dt.datetime.now)
+	out_date = models.DateTimeField()
+	return_date = models.DateTimeField()
+	receiving_user = models.ForeignKey(auth.User, blank=True, null=True)
+	delivering_user = models.ForeignKey(auth.User, blank=True, null=True)
+	returning_person = models.CharField(max_length=100, blank=True, null=True)
+	equipment_list = models.ManyToManyField(
+		equipment.Equipment,
+		limit_choices_to={
+			'status': 'checkout',
+		},
+		related_name='checkouts',
+		blank=True, null=True,
+	)
+	software = models.TextField(blank=True)
+	
+	def __unicode__(self):
+		return u"%s %s, %s" % (
+			self.first_name,
+			self.last_name,
+			self.creation_date
+		)
--- extras/constants.py
+++ extras/constants.py
@@ -34,3 +34,11 @@ FUNDING_SOURCES = (
 	('mc','Media Collections'),
 	('scuf','SCUF'),
 )
+CHECKOUT_TYPES = (
+	('pickup','Pickup'),
+	('delivery','Delivery'),
+)
+RETURNERS = (
+	('requestor','Requestor'),
+	('etc','ETC'),
+)
