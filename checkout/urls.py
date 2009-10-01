f4ba4f5cc516550c7512112145e1502103689352
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-10-01 15:02:14 -0500

    Big update to checkout branch. Index basically done. Working on detail page.

---
+++ checkout/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from etcetera.checkout.views import *
+
+urlpatterns = patterns('',
+	url(r'^$', index, name="checkout-index"),
+	url(r'^(?P<object_id>\d+)/$', detail, name="checkout-detail"),
+	#url(r'^(?P<object_id>\d+)/edit/$', edit, name="equipment-edit"),
+)
