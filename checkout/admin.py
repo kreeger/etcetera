1687da7e30fe0c7bbc1b5104c429ccd292f0ec66
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-10-01 15:02:14 -0500

    Big update to checkout branch. Index basically done. Working on detail page.

---
+++ checkout/admin.py
@@ -0,0 +1,63 @@
+from etcetera.checkout.models import Checkout
+from django.contrib import admin
+
+# This file determines what's shown in the admin interface
+class CheckoutAdmin(admin.ModelAdmin):
+	fieldsets = (
+		('Basic information', {
+			'fields': (
+				'first_name',
+				'last_name',
+				'department',
+				'department_text',
+				'course',
+				'phone',
+				'email',
+				'building',
+				'room',
+				'creation_date',
+				'creating_user',
+			),
+		}),
+		('Checkout information', {
+			'classes': (
+				'collapse',
+			),
+			'fields': (
+				'checkout_type',
+				'return_type',
+				'equipment_needed',
+				'out_date',
+				'return_date',
+				'delivering_user',
+				'returning_person',
+				'equipment_list',
+				'other_equipment',
+				'software',
+				'completed',
+			),
+		}),
+    )
+	list_display = (
+		'first_name',
+		'last_name',
+		'department',
+		'equipment_needed',
+		'checkout_type',
+		'out_date',
+	)
+	list_filter = (
+		'out_date',
+	)
+	search_fields = (
+		'first_name',
+		'last_name',
+		'department__name',
+		'department_text',
+		'building__name',
+		'room',
+		'equipment_needed',
+	)
+
+# Register the appropriate models 
+admin.site.register(Checkout, CheckoutAdmin)
