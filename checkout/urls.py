521c8068f744e02169eba977799a725f90d2fca9
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-10-14 16:30:34 -0500

    Added basic AJAX adding of equipment items to a checkout form. VERY basic.

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 	url(r'^$', index, name="checkout-index"),
 	url(r'^(?P<object_id>\d+)/$', detail, name="checkout-detail"),
 	url(r'^(?P<object_id>\d+)/edit/$', edit, name="checkout-edit"),
+	url(r'^(?P<object_id>\d+)/add_eq/$', add_eq, name="checkout-addeq"),
 )
