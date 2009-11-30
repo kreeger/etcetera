47bcbdc7c09c1c9c7430bdd3583b40b422eca534
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-11-30 11:18:09 -0600

    Whoops. Jacked up my tuples.

--- extras/constants.py
+++ extras/constants.py
@@ -1,7 +1,7 @@
 EQUIPMENT_STATUSES = (
 	('installed','Installed'),
 	('checkout','Checkout'),
-	('checkedout','Checked out')
+	('checkedout','Checked out'),
 	('stock','Stock'),
 	('repair','In repair'),
 	('missing','Missing'),
