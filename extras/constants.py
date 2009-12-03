d534812da30135785e61e2a08477a4a7659f3ce4
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-12-02 21:53:00 -0600

    Added a reserved constant for equipment status.

--- extras/constants.py
+++ extras/constants.py
@@ -1,6 +1,7 @@
 EQUIPMENT_STATUSES = (
 	('installed','Installed'),
 	('checkout','Checkout'),
+	('reserved','Reserved'),
 	('checkedout','Checked out'),
 	('stock','Stock'),
 	('repair','In repair'),
