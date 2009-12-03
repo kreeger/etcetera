4cf7b4f257f7b5a6e5186fa1076ef1e42c6c0ea8
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-12-02 21:56:27 -0600

    The manage.py equipment_status command should be good now. It only checks for current tickets with reserved equipment and sets the equipment status to checkedout.

--- checkout/management/commands/equipment_status.py
+++ checkout/management/commands/equipment_status.py
@@ -1,4 +1,27 @@
-import django.core.management.base.BaseCommand
+import logging
+import datetime
 
-def equipment_status(BaseCommand):
-	pass
+from django.core.management.base import NoArgsCommand
+
+from etcetera.checkout.models import Checkout
+
+class Command(NoArgsCommand):
+	def handle_noargs(self, **options):
+		# Save the current date/time in a variable
+		now = datetime.datetime.now()
+		
+		# Set logging level
+		logging.basicConfig(
+			level=logging.DEBUG,
+			format='%(asctime)s %(levelname)s %(message)s',
+		)
+		
+		# For current checkouts, set related equipment status to checkedout
+		for checkout in Checkout.objects.filter(
+			out_date__lte=now).filter(
+			return_date__gte=now).filter(
+			completed=False):
+			for eq in checkout.equipment_list.all():
+				if eq.status == 'reserved':
+					eq.status = 'checkedout'
+					eq.save()
