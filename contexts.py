9b9f259aa064f36569f1cd93aabcc163871c2525
Benjamin Kreeger <ben@benkreeger.com>
2010-01-29 22:12:03 -0600

    Navigation bar now shows which section you're in (by way of CSS class).

--- contexts.py
+++ contexts.py
@@ -17,3 +17,8 @@ def git_information(request):
 		'current_version': repo.tags[-1].name[1:],
 		'current_commit': repo.commits('master', max_count=1)[0].id[:6]
 	}
+
+def path_info(request):
+	return {
+		'path_info': request.META['PATH_INFO'].split('/')[1],
+	}
