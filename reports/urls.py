b6221f2b8d02e9a56185897763060f38659764d6
Benjamin Kreeger <ben@benkreeger.com>
2010-02-04 15:00:51 -0600

    Changes to some styles, templates, and a rearranged URLconf to account for the edit view.

--- reports/urls.py
+++ reports/urls.py
@@ -4,6 +4,6 @@ from etcetera.reports.views import *
 urlpatterns = patterns('',
 	url(r'^$', index, name="reports-index"),
 	url(r'^new/$', new, name="reports-new"),
-	url(r'^(?P<slug>.*)/$', detail, name="reports-detail"),
-	url(r'^(?P<slug>.*)/edit/$', edit, name="reports-edit"),
+	url(r'^(?P<slug>.+)/edit/$', edit, name="reports-edit"),
+	url(r'^(?P<slug>.+)/$', detail, name="reports-detail"),
 )
