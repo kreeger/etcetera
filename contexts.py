1ae3e48f947d2c318d0336252cabeae8faa4dcc9
Benjamin Kreeger <benjaminkreeger@gmail.com>
2010-03-11 11:23:11 -0600

    Removing need for gitpython.

--- contexts.py
+++ contexts.py
@@ -1,21 +1,12 @@
 # Context processors.
 from django.conf import settings
 
-from git import *
-
 def template_elements(request):
 	# Return a dictionary of values
 	return {
 		'template_header': 'includes/header.html',
 		'template_nav': 'includes/nav.html',
 		'template_footer': 'includes/footer.html',
-	}
-
-def git_information(request):
-	repo = Repo(settings.SITE_ROOT)
-	return {
-		'current_version': repo.tags[-1].name[1:],
-		'current_commit': repo.commits('master', max_count=1)[0].id[:6]
 	}
 
 def path_info(request):
